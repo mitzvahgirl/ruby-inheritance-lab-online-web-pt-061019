696026ae74708e90e0daf3d340e70f48358b9263
Amy Fogelman <abfnj1@gmail.com>
2019-07-28 21:04:40 +0000

    Automatically backed up by Learn

--- lib/teacher.rb
+++ lib/teacher.rb
@@ -1,6 +1,7 @@
 class Teacher < User
   KNOWLEDGE = ["Disney/’s The Lion King is the first Broadway show to gross one billion dollars in revenue", "a Broadway theater must seat at least 500", "Off Broadway can seat between 100-499 people", "On July 25 1975 A Chorus Line opened at the Schubert Theatre on Broadway",
   "Fifteen actors have played the role of the Phantom", 
+  
                "Ruby method call definition",
                "object oriented dog cat class instance",
                "class method class variable instance method instance varaible",
